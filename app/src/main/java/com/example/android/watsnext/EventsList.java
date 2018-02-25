f78db635f47ce7927b0c1c7c2bab69c495bc88c5
Calin <chirilacalincristian@yahoo.com>
2018-02-25 17:28:12 +0200

    Add the Events List layout

---
+++ app/src/main/java/com/example/android/watsnext/EventsList.java
@@ -0,0 +1,30 @@
+package com.example.android.watsnext;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class EventsList extends AppCompatActivity {
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events_list);
+        ButterKnife.bind(this);
+
+        // Setup the toolbar
+        setSupportActionBar(toolbar);
+
+    }
+}
+
+
+//TODO: Check if recyclerView items are placed correctly inside the main screen
+//TODO: FAB should add fake events to the database
+//TODO: Add option to delete entire db from main screen
+//TODO: Add empty state view
