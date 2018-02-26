9dab00dd0d333712cb9367f70697a660d33ed60c
Calin <chirilacalincristian@yahoo.com>
2018-02-26 11:01:14 +0200

    Add the Event class

---
+++ app/src/main/java/com/example/android/watsnext/Event.java
@@ -0,0 +1,123 @@
+package com.example.android.watsnext;
+
+import android.content.Context;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+/**
+ * Created by Calin-Cristian Chirila on 2/26/2018.
+ */
+
+public class Event {
+
+    // Since the event types will be a predefined list, we can store them as integers.
+    private int mEventType;
+
+    // This will be the user's custom description of the event.
+    private String mEventText;
+
+    // Store this as longs. They will be converted into date string for displaying
+    private static long mEventDate;
+    private static long mEventTime;
+
+    private String mEventLocation;
+    // The days of the week when the event should repeat
+    private ArrayList<Integer> mRepeatDays;
+
+    // A boolean that defines if the event has a set reminder. TODO: might convert this to an int if it is more efficient
+    private boolean mHasReminder;
+
+    // The reminder type: 0 => no reminder; 1 => notification; 2 => alarm
+    private int mReminderType;
+
+    // Since we don't have to display reminder time, we can store it as timeInMillis
+    private long mReminderTime;
+
+    public static final long MILLIS_IN_A_DAY = 86400000;
+    public static final long MILLIS_IN_TWO_DAYS = MILLIS_IN_A_DAY * 2;
+
+    // The constructor. Only mandatory fields are date and event type
+    public Event(int eventType, long eventDate){
+        mEventType = eventType;
+        mEventDate = eventDate;
+    }
+
+    /**
+     * Setter methods
+     */
+    public void setEventType(int eventType){
+        mEventType = eventType;
+    }
+
+    public void setEventText(String eventText){
+        mEventText = eventText;
+    }
+
+    public void setEventDate(long eventDate){
+        mEventDate = eventDate;
+    }
+
+    public void setEventTime(long eventTime){
+        mEventTime = eventTime;
+    }
+
+    public void setEventLocation(String eventLocation){
+        mEventLocation = eventLocation;
+    }
+
+    public void setRepeatDays(ArrayList<Integer> repeatDays){
+        mRepeatDays = repeatDays;
+    }
+
+    public void setHasReminder(boolean hasReminder){
+        mHasReminder = hasReminder;
+    }
+
+    public void setReminderType(int reminderType){
+        mReminderType = reminderType;
+    }
+
+    public void setReminderTime(long reminderTime){
+        mReminderTime = reminderTime;
+    }
+
+    /**
+     * Getter methods
+     */
+    public int getEventType(){return mEventType; }
+    public String getEventText(){return mEventText; }
+    public long getEventDate(){return mEventDate;}
+    public long getEventTime(){return mEventTime;}
+    public String getEventLocation(){return mEventLocation;}
+    public ArrayList<Integer> getRepeatDays(){return mRepeatDays;}
+    public boolean getHasReminder(){return mHasReminder;}
+    public int getReminderType(){return mReminderType;}
+    public long getReminderTime(){return mReminderTime;}
+
+
+    // A simple formatter to convert DATE from millis into a readable string
+    public static String convertDateToString(Context context){
+        String formattedDate;
+        Calendar calendar = Calendar.getInstance();
+        long currentTimeInMillis = calendar.getTimeInMillis();
+        long timeBeforeEvent = mEventDate - currentTimeInMillis;
+        if(timeBeforeEvent <= MILLIS_IN_A_DAY){
+            formattedDate = context.getResources().getString(R.string.today);
+        } else if (timeBeforeEvent > MILLIS_IN_A_DAY && timeBeforeEvent <= MILLIS_IN_TWO_DAYS){
+            formattedDate = context.getResources().getString(R.string.tomorrow);
+        } else {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+            formattedDate = dateFormat.format(mEventDate);
+        }
+        return formattedDate;
+    }
+
+    // A simple formatter to convert TIME from millis into a readable string
+    public static String convertTimeToString(){
+        SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm");
+        return timeFormat.format(mEventTime);
+    }
+
+}
--- app/src/main/java/com/example/android/watsnext/EventsList.java
+++ app/src/main/java/com/example/android/watsnext/EventsList.java
@@ -4,6 +4,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -17,13 +22,35 @@ public class EventsList extends AppCompatActivity {
         setContentView(R.layout.activity_events_list);
         ButterKnife.bind(this);
 
+        // Current date and time in millis
+        long currentDate = Calendar.getInstance().getTimeInMillis();
+
+
+
+        /* The input date should be stored as a string to be shown in text view
+         * It should be converted into timeInMillis for calculations then add the equivalent
+         * in millis of event time (if event time unknown, consider it 00:00)
+         * Make 2 constants, MILLIS_IN_A_DAY = 86400000 millis and MILLIS_IN_TWO_DAYS = MILLIS_IN_A_DAY * 2
+         * If input date - current date (in millis) <= MILLIS_IN_A_DAY => event is TODAY
+         * If input date - current date (in millis) > MILLIS_IN_A_DAY but <= MILLIS_IN_TWO_DAYS => event is TOMORROW
+         * Reminder date and time = input date - input reminder
+         */
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        String inputDate = "22-03-2018";
+        try {
+            Date formatInputDate = dateFormat.parse(inputDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
         // Setup the toolbar
         setSupportActionBar(toolbar);
 
     }
 }
 
-
+//TODO: Create the Events class and database
 //TODO: Check if recyclerView items are placed correctly inside the main screen
 //TODO: FAB should add fake events to the database
 //TODO: Add option to delete entire db from main screen
