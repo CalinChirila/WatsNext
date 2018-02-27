0a2b238776fe632156e12a08a5eeeae0ed6bfb30
Calin <chirilacalincristian@yahoo.com>
2018-02-27 12:11:54 +0200

    -DEBUG- make fab add dummy data

--- app/src/main/java/com/example/android/watsnext/EventsList.java
+++ app/src/main/java/com/example/android/watsnext/EventsList.java
@@ -1,10 +1,15 @@
 package com.example.android.watsnext;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.android.watsnext.data.EventContract.EventsEntry;
 
@@ -14,6 +19,8 @@ import butterknife.ButterKnife;
 public class EventsList extends AppCompatActivity {
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    @BindView(R.id.fab_add_event)
+    FloatingActionButton mAddEventButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,32 @@ public class EventsList extends AppCompatActivity {
         toolbar.inflateMenu(R.menu.menu);
         setSupportActionBar(toolbar);
 
+        // Setup the Add Event Button
+        mAddEventButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                // PLACEHOLDER CODE ----------------------------------------------------------------
+                addDummyData();
+                Cursor cursor = getContentResolver().query(EventsEntry.CONTENT_URI, null, null, null, null);
+                Toast.makeText(getApplicationContext(), "Items in database: " + cursor.getCount(), Toast.LENGTH_LONG).show();
+                cursor.close();
+                // ---------------------------------------------------------------------------------
+            }
+        });
+
+    }
+
+    /**
+     * Helper method that adds dummy data to the database
+     * TODO: must delete before release
+     */
+    private void addDummyData(){
+        int eventType = 1;
+        long eventDate = 765132478;
+        ContentValues values = new ContentValues();
+        values.put(EventsEntry.COLUMN_EVENT_TYPE, eventType);
+        values.put(EventsEntry.COLUMN_EVENT_DATE, eventDate);
+        getContentResolver().insert(EventsEntry.CONTENT_URI, values);
     }
 
     @Override
@@ -46,7 +79,7 @@ public class EventsList extends AppCompatActivity {
         return super.onOptionsItemSelected(menuItem);
     }
 }
-
+//TODO: Create recycler view adapter
 //TODO: Check if recyclerView items are placed correctly inside the main screen
 //TODO: FAB should add fake events to the database
 //TODO: Add option to delete entire db from main screen
